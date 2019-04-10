70356f99eccfad7f03015897fc7e46b0dbd39163
brightmanforever022 <brightmanforever@outlook.com>
2019-04-10 13:16:20 -0600

    test production routing

--- server.js
+++ server.js
@@ -71,10 +71,9 @@ router(app);
 // production mode
 if(process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'client/build')));
-  //
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/client/build/index.html'));
-  })
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname + '/client/build/index.html'));
+//   })
 }
 // //build mode
 // app.get('*', (req, res) => {

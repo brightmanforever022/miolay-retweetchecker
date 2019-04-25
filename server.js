d8b0b98aa11774824b9e857bc1e0ce8596b18d38
brightmanforever022 <brightmanforever@outlook.com>
2019-04-25 15:34:59 -0400

    test ssl

--- server.js
+++ server.js
@@ -70,7 +70,10 @@ app.use(express.static(path.join(__dirname, 'client/build')))
 
 // production mode
 if(process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client/build')))
+	app.use(express.static(path.join(__dirname, 'client/build')))
+	app.get('/.well-known/acme-challenge/:content', (req, res) => {
+		res.sendFile(path.join(__dirname + '/public/.well-known/acme-challenge/v6WjfaIvbLhL0kCRmpwxcXwj2Vy0gUIlcgknJndTc9o'))
+	})
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname + '/client/build/index.html'))
   })

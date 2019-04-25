8b267b1719245e027e95559278331486f92b29b8
brightmanforever022 <brightmanforever@outlook.com>
2019-04-25 16:15:57 -0400

    test ssl

---
+++ client/build/precache-manifest.22240a918500eff43dc21c78df9254e3.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "6079da7ba42b778d94cf0b089c79ca63",
+    "url": "/static/media/tool-facebook.6079da7b.png"
+  },
+  {
+    "revision": "4c25ce57a4e605f81da8",
+    "url": "/static/css/main.83212a7b.chunk.css"
+  },
+  {
+    "revision": "7b09407d2a56c2cb81fe",
+    "url": "/static/js/1.7b09407d.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2461026169ccc05420ce32ae72d80bc8",
+    "url": "/static/media/tweet-icons.24610261.svg"
+  },
+  {
+    "revision": "1c5cb81d64e6b8251c8a6dcefd6e8abe",
+    "url": "/static/media/tool-twitter.1c5cb81d.png"
+  },
+  {
+    "revision": "4c25ce57a4e605f81da8",
+    "url": "/static/js/main.4c25ce57.chunk.js"
+  },
+  {
+    "revision": "01f0b0cd054d0db3daf914b84a57206b",
+    "url": "/static/media/tool-facebook-blue.01f0b0cd.png"
+  },
+  {
+    "revision": "15c62144d98e9f316f04dcbeebe52388",
+    "url": "/static/media/tool-twitter-blue.15c62144.png"
+  },
+  {
+    "revision": "97d795675392f5148310e9e2109ef928",
+    "url": "/static/media/tease-mask-bg.97d79567.jpg"
+  },
+  {
+    "revision": "5e8cdd55df9f3a5c071c30f73ce4e0eb",
+    "url": "/static/media/logo.5e8cdd55.svg"
+  },
+  {
+    "revision": "f3185d74b1643474be1639bfea67bcb5",
+    "url": "/index.html"
+  }
+];

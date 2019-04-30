d28c011e94e1574472fe3634dae5be861983f749
brightmanforever022 <brightmanforever@outlook.com>
2019-04-30 22:49:13 +0800

    set back

--- server.js
+++ server.js
@@ -67,7 +67,7 @@ router(app)
 app.use(express.static(__dirname))
 // production mode
 if(process.env.NODE_ENV === 'production') {
-	// app.use(express.static(path.join(__dirname, 'client/build')))
+	app.use(express.static(path.join(__dirname, 'client/build')))
 	app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname + '/client/build/index.html'))
   })
